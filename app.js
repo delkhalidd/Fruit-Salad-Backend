e85edda6a760bec571de3b5386c06e77dbb34744
khalid <tusbaxle11@hotmail.com>
2025-07-15 15:03:05 +0100

    updated

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
 const app = express();
+
 const fruits = require("./routes/fruits")
 
 app.get('/', (req, res) => {
    res.send('Hello Fruity!');
 })
 
+const cors = require("cors")
+app.use(cors())
+app.use(express.json())
 app.use('/fruits', fruits);
 
 module.exports = app
